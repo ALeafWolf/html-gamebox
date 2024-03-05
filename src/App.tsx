0f05063d166fed6593077ba5d572503f60400cf1
AQ <aleafwolf@outlook.com>
2024-03-05 11:32:14 -0500

    project init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Game Box</h1>
+      <ul>
+        <li>2048</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
